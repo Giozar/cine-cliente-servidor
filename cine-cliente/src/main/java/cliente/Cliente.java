88f803c37070bfae226e980572e6fc50f5f9a8f9
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-11-14 16:48:58 -0600

    Init: Inicio de proyecto

---
+++ cine-cliente/src/main/java/cliente/Cliente.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cliente;
+
+/**
+ *
+ * @author giozar
+ */
+public class Cliente {
+
+    public static void main(String[] args) {
+        System.out.println("Hola desde mi cliente");
+    }
+}
---
+++ cine-servidor/src/main/java/servidor/Servidor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package servidor;
+
+/**
+ *
+ * @author giozar
+ */
+public class Servidor {
+
+    public static void main(String[] args) {
+        System.out.println("Hola desde mi servidor");
+    }
+}
